c6bf14a2b1f53ba0a88ca8955cdf1f447ef86d76
Mirko Pampanini <mirkopampanini01@gmail.com>
2024-07-30 16:10:33 +0200

    iniziato

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg bg-body-dark ">
+    <div class="container-fluid">
+        <a class="navbar-brand text-warning" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active text-warning" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-warning" href="#">Il Nostro Locale</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-warning" href="#">Menù</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-warning " href="">Contatti</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
